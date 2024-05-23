719d6e321b6d11cde00be5245b592b310dd865ba
shng03 <168506062+shng03@users.noreply.github.com>
2024-05-23 08:03:58 +0000

    Updated something

---
+++ script.js
@@ -0,0 +1,30 @@
+var age = 20
+age = 65
+
+var username ="sok hwai"
+var password = "techup"
+
+var isMale = true
+var isLoggedIn = false
+
+var blog_post = ["Today was a good day", "The Sky is blue", "It's raining heavily now"]
+
+console.log(age)
+console.log(username)
+console.log(password)
+console.log(isMale)
+console.log("isLoggedIn")
+console.log("blog_post")
+
+if (isMale == true) {
+    console.log("You are male")
+} else {
+    console.log("You are not male")
+}
+while (age < 70) {
+    console.log("Your age is " + age );
+    age = age + 1;
+}
+function sayHello() {
+    alert("Hello everyone");
+}
