20b261a53d387620151e61cf504df4b6c6423899
shng03 <168506062+shng03@users.noreply.github.com>
2024-05-23 09:07:19 +0000

    binking efftect

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-var age = 20
-age = 65
+document.addEventListener('DOMContentLoaded', function() {
+  var videoLinks = document.querySelectorAll('#videoNav a');
+  videoLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+          event.preventDefault(); // Prevent default link behavior
+          var videoId = link.getAttribute('data-video-id');
+          var videoUrl = 'https://www.youtube.com/watch?v=' + videoId;
+          window.open(videoUrl, '_blank'); // Open link in a new window or tab
+      });
+  });
+});
 
-var username ="sok hwai"
-var password = "techup"
+document.addEventListener('DOMContentLoaded', function() {
+  const quotes = [
+    "🤜 Mental health and fitness are like a garden - they require care, nurturing, and regular attention to flourish 🤛",
+    "🤜 Mental health is just as important as physical health. Nurture both with self-care and self-love. 🤛",
+    "🤜 Sweat out the doubts in your mind as you sweat it out in the gym. Mental and physical fitness go hand in hand. 🤛",
+    "🤜 Feed your mind with positive thoughts and nourish your body with physical activity. That's the recipe for mental and fitness success. 🤛",
+    "🤜 In a world where you can be anything, be kind to your mind. 🤛",
+    "🤜 Never apologize for putting yourself first when it comes to your mental health. 🤛", 
+    "🤜 Laughing decreases blood pressure and increases blood flow. 🤛"
+    ];
 
-var isMale = true
-var isLoggedIn = false
+  function generateRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quoteText = document.getElementById('quoteText');
+    quoteText.textContent = quotes[randomIndex];
+    }
 
-var blog_post = ["Today was a good day", "The Sky is blue", "It's raining heavily now"]
+  generateRandomQuote();
+});
 
-console.log(age)
-console.log(username)
-console.log(password)
-console.log(isMale)
-console.log("isLoggedIn")
-console.log("blog_post")
-
-if (isMale == true) {
-    console.log("You are male")
-} else {
-    console.log("You are not male")
+function showPopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'block';
 }
-while (age < 70) {
-    console.log("Your age is " + age );
-    age = age + 1;
+
+function hidePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
 }
-function sayHello() {
-    alert("Hello everyone");
-}
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  showPopup(); 
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+  hidePopup();
+});
+
